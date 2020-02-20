656e50f679f6be3397b226a92429adacacaabde6
Megan <megan.miller@smoldev.dev>
2020-02-20 12:15:18 -0600

    Eating cookies -- during lecture

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,13 +5,15 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
-    if n < 1:
+def eating_cookies(n, cache={1: 1}):
+    if n < 0:
         return 0
-    elif n == 0:
+    if n == 0:
         return 1
-    else:
-        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if n not in cache:
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+
+    return cache[n]
 
 
 if __name__ == "__main__":

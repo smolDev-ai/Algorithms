ec653080d303502013de5d3f73337d34206533d9
Megan <megan.miller@smoldev.dev>
2020-02-20 20:18:42 -0600

    Completing making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,26 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+    # Take in an amount in pennies and return that amount in coins?
+    # would rather do this in an iterative fashion than recursive since I'm still struggling to 
+    # understand recursion.
+
+    # list comprehension for the cache to create a list full of zeros that matches the length of amount + 1
+    cache = [0 for i in range(0, amount+1)]
+    # the first element in the list should be 1
+    cache[0] = 1
+
+    for coins in denominations:
+        for higher_amount in range(coins, amount + 1):
+            # setting the cache's current index to be the sum of itself minus 
+            # whatever coin amount it's on.
+            cache[higher_amount] = cache[higher_amount] + cache[higher_amount-coins]
+    return cache[amount]
+
+
+print(making_change(100, [1, 5, 10, 25, 50]))
+
+
 
 
 if __name__ == "__main__":
